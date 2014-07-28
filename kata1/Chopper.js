65d9bdafc566aeb1391df4e67a15a799a58f17b1
Dominic May <jack.thatch@gmail.com>
2014-07-28 19:40:03 +0800

    Incomplete first kata

---
+++ kata1/Chopper.js
@@ -0,0 +1,28 @@
+﻿function chop(value, values) {
+    debugger;
+    if (values.length === 0) {
+        return -1;
+    }
+
+    var position = values.length,
+        middle = values.length / 2,
+        a = values,
+        b = values;
+
+    while (true) {
+        if (value < a[a.length - 1]) {
+            values = values.slice(0, middle);
+        } else if (value < b[b.length - 1]) {
+            values = values.slice(middle);
+        } else if (value == a[0] || value == b[0]) {
+            return position;
+        }
+
+        if (a == [] && b == []) return -1;
+
+        middle /= 2;
+        position -= middle;
+        a = values.slice(0, middle);
+        b = values.slice(middle);
+    }
+}
---
+++ kata1/SpikeTests.js
@@ -0,0 +1,20 @@
+﻿function NotInArray_ReturnsNegativeOne() {
+    console.assert(-1 === chop(3, [1]));
+}
+
+function SingleValueArray_ValueFound_ReturnsZero() {
+    console.assert(0 === chop(1, [1]));
+}
+
+function MultipleValueArray_ValuetFound_ReturnsZero() {
+    console.assert(0 === chop(1,[1, 3, 5]));
+}
+
+function MultipleValueArray_ValueFound_ReturnsTwo() {
+    console.assert(2 === chop(5, [1, 3, 5, 7]));
+}
+
+NotInArray_ReturnsNegativeOne();
+SingleValueArray_ValueFound_ReturnsZero();
+MultipleValueArray_ValuetFound_ReturnsZero();
+MultipleValueArray_ValueFound_ReturnsTwo();
