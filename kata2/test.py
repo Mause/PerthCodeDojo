91924764564bd0118c44b4d8b5e23c9da40f6b78
Dominic May <jack.thatch@gmail.com>
2014-08-25 18:58:26 +0800

    Refactor

--- kata2/test.py
+++ kata2/test.py
@@ -6,20 +6,20 @@ class TestCheckout(unittest.TestCase):
     def setUp(self):
         self.register = Checkout()
 
+    def assertTotal(self, price):
+        self.assertEqual(self.register.calculate_total(), price)
+
     def test_basic_checkout(self):
         self.register.add_to_cart(Apple(1))
-        self.assertEqual(self.register.calculate_total(), .5)
+        self.assertTotal(.5)
 
     def test_third_free_checkout(self):
         self.register.add_to_cart(Apple(5))
-        self.assertEqual(self.register.calculate_total(), 4 * 0.5)
+        self.assertTotal(4 * 0.5)
 
     def test_seven_apples(self):
         self.register.add_to_cart(Apple(7))
-        self.assertEqual(
-            self.register.calculate_total(),
-            2.5
-        )
+        self.assertTotal(2.5)
 
     def test_multiple_fruit(self):
         self.register.add_multiple([
@@ -27,8 +27,7 @@ class TestCheckout(unittest.TestCase):
             Mango(4),
             Cherry(10)
         ])
-        self.assertEqual(
-            self.register.calculate_total(),
+        self.assertTotal(
             50 +  # cherries 5 * 10
             12 +  # mangoes 3 * 4
             1     # apples 2 * 0.5
