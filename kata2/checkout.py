e624e66065a7dfc9fcf1fb1883827284c6d10c19
Dominic May <jack.thatch@gmail.com>
2014-08-25 18:44:15 +0800

    Scenario 1

--- kata2/checkout.py
+++ kata2/checkout.py
@@ -5,7 +5,45 @@ class Checkout(object):
         self.items = []
 
     def add_to_cart(self, item):
-        pass
+        assert isinstance(item, Item)
+        self.items.append(item)
 
     def calculate_total(self) -> int:
-        pass
+        return sum(
+            item.calc_price()
+            for item in self.items
+        )
+
+
+class Item(object):
+    def __init__(self, name, num, price):
+        self.name = name
+        self.num = num
+        self.price = price
+
+    def calc_price(self):
+        return self.num
+
+
+class Fruit(Item):
+    pass
+
+
+class Apple(Fruit):
+    def __init__(self, num):
+        super().__init__('Apple', num, 0.5)
+
+    def calc_price(self):
+        duos, extra = divmod(self.num, 3)
+
+        return (duos * (2 * 0.5)) + (extra * .5)
+
+
+class Cherry(Fruit):
+    def __init__(self, num):
+        super().__init__('Cherry', num, 5)
+
+
+class Mango(Fruit):
+    def __init__(self, num):
+        super().__init__('Mango', num, 3)
--- kata2/test.py
+++ kata2/test.py
@@ -1,6 +1,29 @@
 import unittest
+from checkout import Apple, Checkout
 
 
 class TestCheckout(unittest.TestCase):
+    def setUp(self):
+        self.register = Checkout()
+
     def test_basic_checkout(self):
-        pass
+        self.register.add_to_cart(Apple(1))
+        self.assertEqual(self.register.calculate_total(), .5)
+
+    def test_third_free_checkout(self):
+        self.register.add_to_cart(Apple(5))
+        self.assertEqual(self.register.calculate_total(), 4 * 0.5)
+
+    def test_seven_apples(self):
+        self.register.add_to_cart(Apple(7))
+        self.assertEqual(
+            self.register.calculate_total(),
+            2.5
+        )
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
